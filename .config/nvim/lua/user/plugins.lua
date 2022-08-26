773efaa5d4e75e6b5bd96f600bdbbfa47f070f4e
ConnerWill <connerwill@connerwill.com>
2022-08-26 17:08:34 -0500

    nvim plugins cmp_zsh

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -67,10 +67,7 @@ return packer.startup(function(use)
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
-
 	use 'nvim-treesitter/nvim-treesitter-context'
-
-
 
 	-- Commenting
 	use("numToStr/Comment.nvim") -- Easily comment stuff
@@ -111,13 +108,13 @@ return packer.startup(function(use)
 	}) })
 
 	-- zsh
-	use({
-		"tamago324/cmp-zsh",
-		require("cmp_zsh").setup({
-			zshrc = true, -- Source the zshrc (adding all custom completions). default: false
-			filetypes = { "deoledit", "zsh" }, -- Filetypes to enable cmp_zsh source. default: {"*"}
-		}),
-	})
+--	use({
+--		"tamago324/cmp-zsh",
+--		require("cmp_zsh").setup({
+--			zshrc = true, -- Source the zshrc (adding all custom completions). default: false
+--			filetypes = { "deoledit", "zsh" }, -- Filetypes to enable cmp_zsh source. default: {"*"}
+--		}),
+--	})
 	-- tmux
 	use({ "andersevenrud/cmp-tmux", require("cmp").setup({
 		sources = {
