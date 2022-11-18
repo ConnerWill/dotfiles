c01b943e4eb514e1f150069d7acc0b4f6765c870
ConnerWill <connerwill@connerwill.com>
2022-11-18 14:20:00 -0600

    fail2ban zsh functions

--- .config/zsh/zsh/user/functions/functions-available/fail2ban-client-status-all.zsh
+++ .config/zsh/zsh/user/functions/functions-available/fail2ban-client-status-all.zsh
@@ -1,5 +1,6 @@
 
 function fail2ban-client-status-all(){
+[[ "${commands[fail2ban]}" || "${commands[fail2ban-client]}" ]] || unfunction fail2ban-client-status-all || printf "Could not find fail2ban"
   sudo echo -n ""
  local PAGER="${PAGER:-less --RAW-CONTROL-CHARS}"
   sudo fail2ban-client status \
@@ -17,14 +18,16 @@ function fail2ban-client-status-all(){
 
 
 function fail2ban-client-dump(){
+[[ "${commands[fail2ban]}" || "${commands[fail2ban-client]}" ]] || unfunction fail2ban-client-dump || printf "Could not find fail2ban"
  sudo echo -n ""
  local PAGER="${PAGER:-less --RAW-CONTROL-CHARS}"
  headertitle='$(printf "\e[0;1;4;38;5;46mFail2Ban:\t\e[1;38;5;87mDUMP\e[0m\n\n\n" ${headertitle})'
  output=$(sudo fail2ban-client -t --dp)
  export jails=$(sudo fail2ban-client status | grep 'Jail list:' | cut -f2- | sed -e 's/\,//g' -e 's/\s/\|/g')
-
- 
  printf "\e[0;1;4;38;5;46mFail2Ban:\t\e[1;38;5;87mDUMP\e[0m\n\n\e[1;38;5;93mJAILS:\e[0m\t\e[0;38;5;190m%s\e[0m\n\n\e[0;1;38;5;33mfail2ban \e[0;1;38;5;201mdump\e[0m\n\n%s\n" "${jails}" "${output}" \
 	   | "${PAGER}"
 	 #| grep -E "($jails)"  \
 }
+
+[[ "${commands[fail2ban]}" || "${commands[fail2ban-client]}" ]] \
+  || unfunction fail2ban-client-dump fail2ban-client-status-all
