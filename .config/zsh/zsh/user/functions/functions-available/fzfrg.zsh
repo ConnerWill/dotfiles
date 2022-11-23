3c1c0ef4ea43a2f5e32d498101fd597e65024595
ConnerWill <connerwill@connerwill.com>
2022-11-23 09:53:27 -0600

    fzfrg

--- .config/zsh/zsh/user/functions/functions-available/fzfrg.zsh
+++ .config/zsh/zsh/user/functions/functions-available/fzfrg.zsh
@@ -17,11 +17,12 @@ function fzfrg() {
 	# ===============================
           COLOR_BG='#0E1224'        COLOR_BGP='#181D31'
           COLOR_FG='#323A5C'        COLOR_FGP='#00FFFF'
+          # COLOR_FG='#323A5C'        COLOR_FGP='#00FFFF'
   COLOR_PREVIEW_FG='#C0CAF5' COLOR_PREVIEW_BG='#202646'
       COLOR_BORDER='#8800EE'       COLOR_INFO='#00FFFF'
-          COLOR_HL='#CF04FA'        COLOR_HLP='#53FFAD' 
+          COLOR_HL='#CF04FA'        COLOR_HLP='#53FFAD'
       COLOR_PROMPT='#AA00FF'     COLOR_HEADER='#0077FF'
-     COLOR_POINTER='#53FFAD'     COLOR_MARKER='#00FF00' 
+     COLOR_POINTER='#53FFAD'     COLOR_MARKER='#00FF00'
 
 	# ===============================
 	#		  Localize Variableles
@@ -39,20 +40,20 @@ function fzfrg() {
 	# ===============================
 	[[ -z "$EDITOR" ]]  \
 		&& EDITOR="nvim"
-	
+
 	[[ -z "$CATCMD" ]] \
 		&& CATCMD="cat"
 
 	[[ -z "$ECHOCMD" ]] \
 		&& ECHOCMD="echo"
-	
+
 	[[ -z "$PAGER" ]] \
 		&& PAGER="less"
-	
+
 #>/dev/null 2>&1
 
 INITIAL_QUERY="${*}"
-	RG_PREFIX="rg --hidden --colors 'match:style:underline' --colors 'match:style:bold' --colors 'match:fg:yellow' --colors 'match:style:intense' --colors 'line:fg:blue' --colors 'column:fg:blue' --colors 'path:style:bold' --colors 'path:style:intense' --colors 'path:fg:white' --no-line-number --no-heading --color=always --smart-case  --with-filename"
+	RG_PREFIX="rg --hidden --line-number --colors 'path:style:bold' --colors 'path:style:intense' --colors 'path:fg:white' --colors 'match:style:underline' --colors 'match:style:bold' --colors 'match:fg:yellow' --colors 'match:style:intense' --colors 'line:fg:blue' --colors 'column:fg:blue' --no-heading --color=always --with-filename --iglob '*.cache*'  --iglob '*cache*'  --iglob '.cache'  --iglob '*.bak'  --iglob '*history*' --iglob '*history' --iglob '*compdump*' --iglob '*compcache*' --regexp"   #  --engine=pcre2
 	#RG_PREFIX="rg --column --hidden --colors 'match:style:underline' --colors 'match:style:bold' --colors 'match:style:intense' --colors 'line:fg:blue' --colors 'column:fg:blue' --line-number --no-heading --color=always --smart-case "
 	FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'"
 	# ===============================
@@ -75,7 +76,7 @@ INITIAL_QUERY="${*}"
 		--color=prompt:${COLOR_PROMPT},hl+:${COLOR_HLP} \
 		--color=border:${COLOR_BORDER} \
 		--color=preview-fg:${COLOR_PREVIEW_FG},preview-bg:${COLOR_PREVIEW_BG} \
-		--bind "change:reload:${RG_PREFIX} {q} || true" \
+		--bind "change:reload:${RG_PREFIX} '{q}' || true" \
 		--bind "ctrl-p:execute(${PAGER} {1})" \
 		--bind 'ctrl-/:toggle-preview' \
 		--bind 'pgdn:preview-page-down'  \
