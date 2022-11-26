167aded0f103c2b2bd4acb614b5e90b045d097b1
ConnerWill <connerwill@connerwill.com>
2022-11-26 14:46:46 -0600

    cht

--- .config/zsh/zsh/user/functions/functions-available/cht.zsh
+++ .config/zsh/zsh/user/functions/functions-available/cht.zsh
@@ -4,7 +4,6 @@ function cht(){
     printf "Searching for %s ...\n" "${i}"
     chtoutput+=$(printf "\n\n\e[0;1;4;38;5;46m%s\e[0m\n\n" "${i}")
     chtoutput+=$(curl --silent "https://cht.sh"/"${i}")
-    # curl --silent "https://cht.sh"/"${i}" | bat --plain --language=Manpage
   done
-  echo -e "${chtoutput}" | "${PAGER:-less}"
+  echo -e "${chtoutput}" | "${PAGER:-${commands[cat]}}"
 }
