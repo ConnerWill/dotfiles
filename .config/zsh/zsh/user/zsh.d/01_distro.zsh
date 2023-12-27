e4b57919108b4a559c61388a7e9da3fbcf34ec31
Conner.Will <conner.will@infinitecampus.com>
2023-12-27 16:05:20 -0600

    Fixed macos ostype catch

--- .config/zsh/zsh/user/zsh.d/01_distro.zsh
+++ .config/zsh/zsh/user/zsh.d/01_distro.zsh
@@ -4,6 +4,8 @@ if [[ "${OSTYPE}" == "linux-gnu" ]]; then
   [[ -f /etc/os-release ]] && export DISTRO=$(cat /etc/os-release | grep --extended-regexp --regexp='^NAME=' | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'"' -f2)
 elif [[ "${OSTYPE}" == "linux-android" ]]; then
   export DISTRO="Android"
+elif [[ "${OSTYPE}" == "darwin"* ]]; then
+  export DISTRO="Darwin"
 else
   unset DISTRO
 fi
