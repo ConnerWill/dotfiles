a866c94d8975735b8b913e132ccdae7c29dad5b0
Conner.Will <conner.will@infinitecampus.com>
2024-01-23 14:40:40 -0600

    Added another os definiton to be able to detect linux-musl OSTYPE

--- .config/zsh/zsh/user/zsh.d/01_distro.zsh
+++ .config/zsh/zsh/user/zsh.d/01_distro.zsh
@@ -2,20 +2,26 @@
 
 case "${OSTYPE}" in
   "linux-gnu")
-    if [[ -f /etc/os-release ]]; then
-      DISTRO=$(grep --extended-regexp --regexp='^NAME=' /etc/os-release | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'"' -f2)
-      export DISTRO
-    fi
+      if [[ -f /etc/os-release ]]; then
+        DISTRO=$(grep --extended-regexp --regexp='^NAME=' /etc/os-release | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'"' -f2)
+        export DISTRO
+      fi
+    ;;
+  "linux-musl")
+      if [[ -f /etc/os-release ]]; then
+        DISTRO=$(grep --extended-regexp --regexp='^NAME=' /etc/os-release | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'"' -f2)
+        export DISTRO
+      fi
     ;;
   "linux-android")
-    DISTRO="Android"
-    export DISTRO
+      DISTRO="Android"
+      export DISTRO
     ;;
   "darwin"*)
-    DISTRO="Darwin"
-    export DISTRO
+      DISTRO="Darwin"
+      export DISTRO
     ;;
   *)
-    unset DISTRO
+      unset DISTRO
     ;;
 esac
