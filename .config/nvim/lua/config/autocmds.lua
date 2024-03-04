015c6b33ea025642c1ca0044b593a0d9756dfda0
Conner.Will <conner.will@infinitecampus.com>
2024-03-04 12:07:51 -0600

    Added autosave for vim but commented it out

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -41,11 +41,28 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+-- Auto Save
+--[[
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
 
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
 
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
 
-
-
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
+})
+--]]
 
 --[[ DEFAULTS
 
