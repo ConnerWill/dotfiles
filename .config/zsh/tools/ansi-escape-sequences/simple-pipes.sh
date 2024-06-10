b1bc5aa11a4cf3d12ce1699c9e1d7c9acd526847
ConnerWill <conner.will@outlook.com>
2024-06-10 10:31:37 -0500

    .

--- .config/zsh/tools/ansi-escape-sequences/simple-pipes.sh
+++ .config/zsh/tools/ansi-escape-sequences/simple-pipes.sh
@@ -5,7 +5,7 @@ typeset -il f x s
 typeset -il w
 typeset -al v
 
-f=80 s=15 r=2000 t=0 c=1 n=0 l=0
+f=80 s=15 r=999999999 t=0 c=1 n=0 l=0
 w=$(tput cols) h=$(tput lines)
 x=$(( w/2 )) y=$(( h/2 ))
 v=( [00]="\x83" [01]="\x8f" [03]="\x93" [10]="\x9b" [11]="\x81" [12]="\x93" [21]="\x97" [22]="\x83" [23]="\x9b" [30]="\x97" [32]="\x8f" [33]="\x81" )
@@ -35,7 +35,7 @@ OPTIONS
 
   -f|--framerate INT    Framerate (20-100) (Default:${f})
   -p|--straight  INT    Probability of a straight fitting (5-50) (Default:15)
-  -r|--reset LIMIT      Reset after n characters (Default:2000)
+  -r|--reset LIMIT      Reset after n characters (Default:999999999)
   -h|--help             Show help
 
 HELPMENU
@@ -48,7 +48,7 @@ OPTIND=1
 while getopts "f:s:r:h" arg; do
   case $arg in
     f*|-f*) (( f=( OPTARG > 19 && OPTARG < 101 ) ?OPTARG:f )) ;;
-    *|-s*) (( s=( OPTARG >  4 && OPTARG < 50  ) ?OPTARG:s )) ;;
+    p*|-s*) (( s=( OPTARG >  4 && OPTARG < 50  ) ?OPTARG:s )) ;;
     r*|-r*) (( r=( OPTARG >  0                 ) ?OPTARG:r )) ;;
     h*) helpmenu; exit 0                             ;;
   esac
