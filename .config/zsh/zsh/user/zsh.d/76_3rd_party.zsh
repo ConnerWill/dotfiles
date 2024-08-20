63126d6f35ddd4374c3cce978d8c862abea9bfc9
ConnerWill <conner.will@outlook.com>
2024-08-20 09:56:45 -0500

    Updated

--- .config/zsh/zsh/user/zsh.d/76_3rd_party.zsh
+++ .config/zsh/zsh/user/zsh.d/76_3rd_party.zsh
@@ -3,18 +3,16 @@
 ### [~].......... SHELLFISH
 ### [=]==================================[=]
 
-SHELLFISHRC="${SHELLFISHRC:-${HOME}/.shellfishrc}"
-SHELLFISH_CONFIG_DIR="${SHELLFISH_CONFIG_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/shellfish}"
-export SHELLFISHRC SHELLFISH_CONFIG_DIR
-
 if [[ -n "${LC_TERMINAL}" ]]; then
+  SHELLFISHRC="${SHELLFISHRC:-${HOME}/.shellfishrc}"
+  SHELLFISH_CONFIG_DIR="${SHELLFISH_CONFIG_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/shellfish}"
+  export SHELLFISHRC SHELLFISH_CONFIG_DIR
   if [[ -f "${SHELLFISHRC}" ]]; then
     if [[ ! -d "${SHELLFISH_CONFIG_DIR}" ]]; then
-      mkdir -pv "${SHELLFISH_CONFIG_DIR}" 
+      mkdir -pv "${SHELLFISH_CONFIG_DIR}"
     fi
-    source "${SHELLFISHRC}" || echo "cant source shellfishrc"
+    source "${SHELLFISHRC}" || printf "cant source ShellFish config file: %s\n" "${SHELLFISHRC}"
   else
     unset SHELLFISHRC SHELLFISH_CONFIG_DIR
   fi
 fi
-
