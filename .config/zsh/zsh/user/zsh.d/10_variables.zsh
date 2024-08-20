abf69c4ad7d9f640f6cc3a2a2e27530ea6808a0a
ConnerWill <conner.will@outlook.com>
2024-08-20 10:34:17 -0500

    Updated

--- .config/zsh/zsh/user/zsh.d/10_variables.zsh
+++ .config/zsh/zsh/user/zsh.d/10_variables.zsh
@@ -50,9 +50,9 @@ if [[ -n "${DISPLAY}" ]]; then
 	### [=]==================================[=]
 	### [~]............ BROWSER
 	### [=]==================================[=]
-	[[ "${commands[librewolf]}"   ]] && BROWSER="icecat"       ## Set the order of prefered browsers if when using a WM.
-	[[ "${commands[icecat]}"      ]] && BROWSER="icecat"       ## Most prefered browser should go at the bottom of this first 'if' statement.
-	[[ "${commands[tor-browser]}" ]] && BROWSER="tor-browser"  ## (Default: FireFox is prefered. then tor, then icecat ... etc)
+	[[ "${commands[librewolf]}"   ]] && BROWSER="icecat"       ## Set the order of preferred browsers if when using a WM.
+	[[ "${commands[icecat]}"      ]] && BROWSER="icecat"       ## Most preferred browser should go at the bottom of this first 'if' statement.
+	[[ "${commands[tor-browser]}" ]] && BROWSER="tor-browser"  ## (Default: FireFox is preferred. then tor, then icecat ... etc)
 	[[ "${commands[firefox]}"     ]] && BROWSER="firefox"
 
 	### [=]==================================[=]
@@ -123,7 +123,7 @@ else
 			[[ "${commands[lynx]}"     ]] && BROWSER="lynx"
 			[[ "${commands[xdg-open]}" ]] && BROWSER="xdg-open"
 		fi
-	else 	                                                                ## Otherwise try lynxs and xdg-open
+	else                                                               ## Otherwise try lynxs and xdg-open
 	  [[ "${commands[lynx]}"     ]] && BROWSER="lynx"
 	  [[ "${commands[xdg-open]}" ]] && BROWSER="xdg-open"
 	fi
@@ -282,11 +282,12 @@ fi
 ### [~]............ TMUX
 ### [=]==================================[=]
 if [[ "${commands[tmux]}" ]]; then
-	export TSM_HOME="${XDG_DATA_HOME}/tsm"
-	export TSM_SESSIONS_DIR="${TSM_HOME}/sessions"
-	export TSM_BACKUPS_DIR="${TSM_HOME}/backups"
-	export TSM_DEFAULT_SESSION_FILE="${TSM_HOME}/default-session.txt"
-	export TSM_BACKUPS_COUNT=30
+	TSM_HOME="${XDG_DATA_HOME}/tsm"
+	TSM_SESSIONS_DIR="${TSM_HOME}/sessions"
+	TSM_BACKUPS_DIR="${TSM_HOME}/backups"
+	TSM_DEFAULT_SESSION_FILE="${TSM_HOME}/default-session.txt"
+	TSM_BACKUPS_COUNT=30
+  export TSM_HOME TSM_SESSIONS_DIR TSM_BACKUPS_DIR TSM_DEFAULT_SESSION_FILE TSM_BACKUPS_COUNT
 fi
 
 ### [=]==================================[=]
@@ -308,6 +309,26 @@ fi
 ### [=]==================================[=]
 if [[ "${commands[packer]}" ]]; then
 	export CHECKPOINT_DISABLE=1
+fi
+
+### [=]==================================[=]
+### [~]............ TERRAFORM
+### [=]==================================[=]
+if [[ "${commands[terraform]}" ]]; then
+  TF_CLI_CONFIG_FILE="${XDG_CONFIG_HOME}/terraform/terraform_config.tfrc"
+  TERRAFORM_CONFIG="${TF_CLI_CONFIG_FILE}"
+  TF_CLI_ARGS_apply='-auto-approve'
+  export TF_CLI_CONFIG_FILE TERRAFORM_CONFIG TF_CLI_ARGS_apply
+fi
+
+### [=]==================================[=]
+### [~]............ SPACECTL
+### [=]==================================[=]
+if [[ "${commands[spacectl]}" ]]; then
+  SPACELIFT_API_KEY_ENDPOINT="" # the URL to your Spacelift account
+  SPACELIFT_API_KEY_ID="" # the ID of your Spacelift API key. Available via the Spacelift application
+  SPACELIFT_API_KEY_SECRET="" # the secret for your API key. Only available when the secret is created
+  export SPACELIFT_API_KEY_ENDPOINT SPACELIFT_API_KEY_ID SPACELIFT_API_KEY_SECRET
 fi
 
 ### [=]==================================[=]
