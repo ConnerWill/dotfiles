084c9104e36dfb3fe4047a88cbe3e289c237339e
ConnerWill <conner.will@outlook.com>
2024-08-30 10:47:01 -0500

    MANPATH

--- .config/zsh/zsh/user/functions/functions-available/count-characters.zsh
+++ .config/zsh/zsh/user/functions/functions-available/count-characters.zsh
@@ -1,7 +1,7 @@
 function count-characters(){
   local input_item num_characters
   for input_item in "${@}"; do
-    num_characters=$(printf "%s" "${input_item}" | wc -c)
+    num_characters=$(printf "%s" "${input_item}" | wc --chars)
     printf "%s :\t%s\n" "${input_item}" "${num_characters}"
   done
 }
--- .config/zsh/zsh/user/zsh.d/10_variables.zsh
+++ .config/zsh/zsh/user/zsh.d/10_variables.zsh
@@ -148,23 +148,25 @@ fi
 ### [=]==================================[=]
 ### [~]........... MAN
 ### [=]==================================[=]
-MANPAGER='nvim +Man!'
-MANWIDTH="$(( COLUMNS - (( COLUMNS / 4 )) ))"
-
-man() {
-	export MANWIDTH="$(( COLUMNS ))"
-    env \
-        LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_md="$(printf "\e[1;35m")" \
-        LESS_TERMCAP_me="$(printf "\e[0m")" \
-        LESS_TERMCAP_se="$(printf "\e[0m")" \
-        LESS_TERMCAP_so="$(printf "\e[4;36m")" \
-        LESS_TERMCAP_ue="$(printf "\e[0m")" \
-        LESS_TERMCAP_us="$(printf "\e[3;34m")" \
-        PAGER="${commands[less]:-${PAGER}}" \
-				MANWIDTH="$(( COLUMNS ))" \
-    man "${@}"
-}
+if [[ -n "${commands[man]}" ]] ; then
+  MANPAGER='nvim +Man!'
+  MANWIDTH="$(( COLUMNS - (( COLUMNS / 4 )) ))"
+  MANPATH=$(man --path)
+  man() {
+    export MANWIDTH="$(( COLUMNS ))"
+      env \
+          LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
+          LESS_TERMCAP_md="$(printf "\e[1;35m")" \
+          LESS_TERMCAP_me="$(printf "\e[0m")" \
+          LESS_TERMCAP_se="$(printf "\e[0m")" \
+          LESS_TERMCAP_so="$(printf "\e[4;36m")" \
+          LESS_TERMCAP_ue="$(printf "\e[0m")" \
+          LESS_TERMCAP_us="$(printf "\e[3;34m")" \
+          PAGER="${commands[less]:-${PAGER}}" \
+          MANWIDTH="$(( COLUMNS ))" \
+      man "${@}"
+  }
+fi
 
 ## [=]==================================[=]
 ## [~]............ PAGER
