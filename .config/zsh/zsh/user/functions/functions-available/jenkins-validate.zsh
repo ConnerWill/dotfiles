798378a1bdab975f3230e01afb389644526c39db
ConnerWill <conner.will@outlook.com>
2024-10-08 14:45:38 -0500

    jenkins-validate

--- .config/zsh/zsh/user/functions/functions-available/jenkins-validate.zsh
+++ .config/zsh/zsh/user/functions/functions-available/jenkins-validate.zsh
@@ -3,14 +3,14 @@
 
 function jenkins-validate(){
   ## Define input file
-  local jenkins_file="${1:-Jenkinsfile}" 
-  
-  ## Check if all required variables are set
-  [[ -z "${JENKINS_AUTH}" ]] && printf "JENKINS_AUTH is not defined\n" && return 1
-  [[ -z "${JENKINS_URL}"  ]] && printf "JENKINS_URL is not defined\n"  && return 1
+  local jenkins_file="${1:-Jenkinsfile}"
 
   ## Check if Jenkinsfile exists
   [[ -f "${jenkins_file}" ]] || printf "Cannot find Jenkinsfile: %s\n" "${jenkins_file}" || return 1
+
+  ## Check if all required variables are set
+  [[ -z "${JENKINS_AUTH}" ]] && printf "JENKINS_AUTH is not defined\n" && return 1
+  [[ -z "${JENKINS_URL}"  ]] && printf "JENKINS_URL is not defined\n"  && return 1
 
   ##  These instructions assume that the security realm of Jenkins is something other than "None" and you have an account.
   ##  JENKINS_URL=[root URL of Jenkins controller]
