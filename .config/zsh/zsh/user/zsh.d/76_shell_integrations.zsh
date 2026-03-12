c9f41e2ad02926ed910ba386bedd5b390ce37353
ConnerWill <conner.will@outlook.com>
2026-03-12 00:15:14 -0500

    shellfish

--- .config/zsh/zsh/user/zsh.d/76_shell_integrations.zsh
+++ .config/zsh/zsh/user/zsh.d/76_shell_integrations.zsh
@@ -1,18 +1,29 @@
-#shellcheck disable=2148,2015
 ### [=]==================================[=]
 ### [~].......... SHELLFISH
 ### [=]==================================[=]
 
-if [[ -n "${LC_TERMINAL}" ]]; then
-  SHELLFISHRC="${SHELLFISHRC:-${HOME}/.shellfishrc}"
-  SHELLFISH_CONFIG_DIR="${SHELLFISH_CONFIG_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/shellfish}"
-  export SHELLFISHRC SHELLFISH_CONFIG_DIR
-  if [[ -f "${SHELLFISHRC}" ]]; then
-    if [[ ! -d "${SHELLFISH_CONFIG_DIR}" ]]; then
-      mkdir -pv "${SHELLFISH_CONFIG_DIR}"
-    fi
-    source "${SHELLFISHRC}" || printf "cant source ShellFish config file: %s\n" "${SHELLFISHRC}"
-  else
-    unset SHELLFISHRC SHELLFISH_CONFIG_DIR
-  fi
+# if [[ -n "${LC_TERMINAL}" ]]; then
+#   SHELLFISHRC="${SHELLFISHRC:-${HOME}/.shellfishrc}"
+#   SHELLFISH_CONFIG_DIR="${SHELLFISH_CONFIG_DIR:-${XDG_CONFIG_HOME:-${HOME}/.config}/shellfish}"
+#   export SHELLFISHRC SHELLFISH_CONFIG_DIR
+#   if [[ -f "${SHELLFISHRC}" ]]; then
+#     if [[ ! -d "${SHELLFISH_CONFIG_DIR}" ]]; then
+#       mkdir -pv "${SHELLFISH_CONFIG_DIR}"
+#     fi
+#     source "${SHELLFISHRC}" || printf "cant source ShellFish config file: %s\n" "${SHELLFISHRC}"
+#   else
+#     unset SHELLFISHRC SHELLFISH_CONFIG_DIR
+#   fi
+# fi
+
+#
+#
+#
+#
+#
+#
+SHELLFISHRC="${SHELLFISHRC:-${HOME}/.shellfishrc}"
+export SHELLFISHRC
+if [[ -e "${SHELLFISHRC}" ]]; then
+  source "${SHELLFISHRC}" || printf "cant source ShellFish config file: %s\n" "${SHELLFISHRC}"
 fi
